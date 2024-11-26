7fda2c7830c16172ce94f6fccde75e86b8c43220
Hitanshu-Variya <167282735+Hitanshu-Variya@users.noreply.github.com>
2024-11-26 17:38:49 +0530

    Update GenerateJWTTokenAndCookies.ts for deployment

--- backend/Utilities/GenerateJWTTokenAndCookies.ts
+++ backend/Utilities/GenerateJWTTokenAndCookies.ts
@@ -12,8 +12,8 @@ const GenerateJWTTokenAndCookie = (userID:Types.ObjectId, res:Response) => {
     res.cookie("jwt", token, {
         maxAge: 10*24*60*60*1000,
         httpOnly: true,
-        sameSite: process.env.MODE !== "development" ? "none" : false,
-        secure: process.env.MODE !== "development"
+        sameSite: 'None',
+        secure: true
     })
 };
 
