dbfdea2c0db6a07279b222112473e1263f9dcd42
Hitanshu-Variya <167282735+Hitanshu-Variya@users.noreply.github.com>
2024-11-27 15:30:20 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,19 +19,8 @@ app.use(cors({
   methods: 'GET,POST,PUT,DELETE,OPTIONS',
   credentials: true
 }));
-app.use((req, res, next) => {
-    const allowedOrigin = process.env.CLIENT_URL; 
-    res.header("Access-Control-Allow-Origin", allowedOrigin);
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS"); 
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); 
-    res.header("Access-Control-Allow-Credentials", "true"); 
 
-    if (req.method === "OPTIONS") {
-        return res.sendStatus(204);
-    }
-    next();
-});
-
+app.options('*', cors());
 app.get('/', (req, res) => {
     res.send("Backend Working!")
 })
