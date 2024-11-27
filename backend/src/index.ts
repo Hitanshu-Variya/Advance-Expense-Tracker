b05fd62a89654545e5ae4e6569ddc9c5431f066c
Hitanshu-Variya <167282735+Hitanshu-Variya@users.noreply.github.com>
2024-11-27 16:05:41 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ import cors from 'cors';
 import authRoutes from "../Routes/auth.routes";
 import transactionRoutes from "../Routes/transaction.routes";
 import budgetRoutes from "../Routes/budget.routes";
-import ConnectToDB from "../Database/ConnectToDB"
+import ConnectToDB from "../Database/ConnectToDB";
 import cookieParser from 'cookie-parser';
 import profileRoutes from "../Routes/profile.routes";
 
@@ -12,29 +12,19 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(express.json());
-app.use(cookieParser());
 app.use(cors({
     origin: process.env.CLIENT_URL,
-  methods: 'GET,POST,PUT,DELETE,OPTIONS',
-  credentials: true
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
 }));
+app.use(express.json());
+app.use(cookieParser());
 
-app.use((req, res, next) => {
-    console.log("Incoming Origin:", req.headers.origin); 
-    next();
-});
+app.options('*', cors());
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
 app.get('/', (req, res) => {
-    res.send("Backend Working!")
-})
+    res.send("Backend Working!");
+});
 app.use("/api/auth", authRoutes);
 app.use("/data", transactionRoutes);
 app.use('/budget', budgetRoutes);
