b4f0d683ed2f0f3a7b03bbe204ca9fa0fe8ace1a
Hitanshu-Variya <167282735+Hitanshu-Variya@users.noreply.github.com>
2024-11-27 15:44:47 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,7 +21,7 @@ app.use(cors({
 }));
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
