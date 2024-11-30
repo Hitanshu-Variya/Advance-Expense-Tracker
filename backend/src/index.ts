4a43375d95cbcc5c833608a048cb778b519ab3c8
Hitanshu Variya <hjvariya1@gmail.com>
2024-11-30 15:11:03 +0530

    Changed Cors Configs!

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,19 +12,9 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-  methods: 'GET,POST,PUT,DELETE,OPTIONS',
-  credentials: true
-}));
-app.use((_, res, next) => {
-    res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
-    res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
 
 app.get('/', (req, res) => {
     res.send("Backend Working!")
