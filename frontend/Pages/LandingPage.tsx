0cadf55648a1a32d1a9f34c98b7532dc0b55ca0d
Hitanshu-Variya <167282735+Hitanshu-Variya@users.noreply.github.com>
2025-07-04 15:47:05 +0530

    Update LandingPage.tsx

--- frontend/Pages/LandingPage.tsx
+++ frontend/Pages/LandingPage.tsx
@@ -6,19 +6,26 @@ import { useRef } from 'react';
 
 const LandingPage = () => {
   const featuresRef = useRef<HTMLElement | null>(null);
+  const ContactRef = useRef<HTMLElement | null>(null);
 
   const scrollToFeatures = () => {
     featuresRef.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
+  const scrollToContact = () => {
+    ContactRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className="mx-auto px-4 md:px-14 pb-6 bg-black overflow-hidden scroll-smooth">
-      <Navbar onFeaturesClick={scrollToFeatures} />
+      <Navbar onFeaturesClick={scrollToFeatures} onContactClick={scrollToContact} />
       <HeroSection />
       <section ref={featuresRef} id="features">
         <FeatureSection />
       </section>
-      <FooterSection />
+      <section ref={ContactRef} id="contact-us">
+        <FooterSection />
+      </section>
     </div>
   );
 }
